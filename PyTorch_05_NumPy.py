7551f7a7bc28f855505f7e44fccb48dabc469176
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-04-02 12:48:23 -0400

    Update PyTorch_05_NumPy.py

--- PyTorch_05_NumPy.py
+++ PyTorch_05_NumPy.py
@@ -7,7 +7,7 @@ tensor_torch = torch.tensor([[1,2,3],[4,5,6]])
 # Tensor to NumPy matrix
 tensor_numpy = tensor_torch.numpy()
 print('NumPy matrix:\n', tensor_numpy)
-a
+
 # NumPy matrix to Tensor
 tensor_torch = torch.from_numpy(tensor_numpy)
 print('PyTorch tensor:\n', tensor_torch)
