aa1b87e2d9ce587141db2b92bbac1353051130d6
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-04-02 12:42:20 -0400

    Update PyTorch_02_Reshape.py

---
+++ PyTorch_02_Reshape.py
@@ -0,0 +1,9 @@
+import torch
+
+# Initializing a tensor with elements from 0 to 12
+tensor_1 = torch.arange(12)
+print('Tensor:\n', tensor_5)
+
+# Reshaping the tensor to 3x4
+tensor_2 = tensor_1.reshape(3,4)
+print('Tensor reshaped:')
